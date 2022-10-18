4c23c7b0187b946ed12b3670db5332573e057eaf
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2022-10-19 00:06:28 +0530

    Initial Faq

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Project FSJS - Accordian FAQ
+
+## This is created by me.(Mo Ansar Mansoorie)
+
+![badge](https://img.shields.io/badge/Project%20FSJS-Accordian%20FAQ-yellow)
+
+[Link of live project for view]()
+
+![View page](./completedPage.png)
+
+# Howmany Time will be consumed to making this project.
+
+- This project have taken 1 hours to completed.
---
+++ script.js
@@ -0,0 +1,15 @@
+// const accordian = document.getElementsByClassName("questio0n");
+
+// for (i = 0; i<accordian.length; i++) {
+//     accordian[i].addEventListener('click', function() {
+//         this.classList.toggle('active');
+//     })
+// }
+
+// const faqs = document.querySelectorAll(".contentbox");
+
+// faqs.forEach(faq => {
+//     faq.addEventListener("click", () => {
+//         faq.classList.toggle("active");
+//     })
+// })
